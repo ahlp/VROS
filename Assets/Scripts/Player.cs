008c4ef2eeb7143538bf4147b82b842f1da1fbf0
Arthur Pires <Arthur Pires>
2016-05-13 22:57:11 -0300

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Photon.MonoBehaviour {
+
+	public float speed = 10f;
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (photonView.isMine) InputMovement();
+	}
+
+	void InputMovement()
+	{
+		if (Input.GetKey(KeyCode.W))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.forward * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.S))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.forward * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.D))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + Vector3.right * speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.A))
+			GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * speed * Time.deltaTime);
+	}
+
+	/*void OnJoinedRoom()
+	{
+		// Spawn player
+		PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+	}*/
+}
