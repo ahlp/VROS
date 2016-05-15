9eac81405d6c6c374c5c7a19aae8f0192f008117
Arthur Pires <Arthur Pires>
2016-05-15 13:08:46 -0300

    now its possible enter in a room and move with others players normal

---
+++ Assets/addCameraVR.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addCameraVR : Photon.MonoBehaviour
+{
+    public GameObject camera;
+	// Use this for initialization
+	void Start () {
+        bool localObject = photonView.isMine;
+        
+        if (localObject)
+        {
+            Object newCam = Instantiate(camera, transform.position, transform.rotation);
+            var newTransform = ((GameObject)newCam).transform;
+            newTransform.parent = transform;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
